3ea1868acd5f4d21e078d1b0187787c00ea77512
Julien879465 <beillonnet@laposte.net>
2023-01-17 11:59:00 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+console.log(`My name is ${process.env.MY_NAME}`);
+console.log(
+  `I live in  ${process.env.MY_CITY} `
+);
+console.log(
+  `My language is ${process.env.MY_LANGUAGE}`
+);
